49172d544fd5a429827ffd316b2e51306046f109
syx9527 <1275241305@q.com>
2022-03-01 14:41:23 +0800

    upload SE

--- SE/hello-app/src/com/syx/hello/HelloWorld.java
+++ SE/hello-app/src/com/syx/hello/HelloWorld.java
@@ -1,4 +1,8 @@
 package com.syx.hello;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
 }

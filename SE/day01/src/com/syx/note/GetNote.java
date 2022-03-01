0e01aa86fb7bd42545dd48d6758e5d93215ea389
syx9527 <1275241305@q.com>
2022-03-01 17:16:53 +0800

    upload day01

--- SE/day01/src/com/syx/note/GetNote.java
+++ SE/day01/src/com/syx/note/GetNote.java
@@ -1,4 +1,22 @@
 package com.syx.note;
 
+class GetTwo {
+    static String d(int num) {
+        String res = "";
+        int a = 0;
+        for (; num > 1; ) {
+            a = num % 2;
+            num = num / 2;
+
+        }
+        return res;
+    }
+}
+
 public class GetNote {
+    public static void main(String[] args) {
+        String res = GetTwo.d(9);
+        System.out.println(res);
+    }
+
 }

fd4ec4a0495e1fbc40eefb999bc91b44d009da8b
SYX <1275241305@QQ.COM>
2022-03-04 00:35:04 +0800

    upload day02.test

--- day02/src/com/syx/param/MethodDemo5.java
+++ day02/src/com/syx/param/MethodDemo5.java
@@ -1,25 +1,12 @@
 package com.syx.param;
 
-import java.util.Random;
-
 public class MethodDemo5 {
     public static void main(String[] args) {
-        int n, r;
-        n = 5;
-        r = 10;
+
 
         int[] arr1 = {1, 2, 3}, arr2 = {1, 2, 3};
         System.out.println(arrayIs(arr1, arr2) ? "一样" : "不一样");
 
-    }
-
-    public static int[] getRandomArray(int n, int r) {
-        int[] arr = new int[n];
-        Random ran = new Random();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = ran.nextInt(r);
-        }
-        return arr;
     }
 
     public static boolean arrayIs(int[] arr1, int[] arr2) {

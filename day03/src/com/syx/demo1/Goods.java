5b53783bd16360c76e6bdab117a5f1066d8c5cf1
SYX <1275241305@QQ.COM>
2022-03-05 17:24:49 +0800

    new day03

---
+++ day03/src/com/syx/demo1/Goods.java
@@ -0,0 +1,10 @@
+package com.syx.demo1;
+
+public class Goods {
+    int id; // 编号
+    String name; // 名称
+    double price; // 价格
+    int buyNumber; // 购买数量
+
+
+}
---
+++ day03/src/com/syx/demo1/test.java
@@ -0,0 +1,10 @@
+package com.syx.demo1;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("\t\t".length());
+        System.out.println( "哈利波特".length());
+
+
+    }
+}

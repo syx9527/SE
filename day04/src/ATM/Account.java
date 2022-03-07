aff53b59e1f27cd1d3ac6220869aa7424787bc83
syx9527 <1275241305@q.com>
2022-03-07 12:38:27 +0800

    修正编译器没有识别jdk语言版本

--- day04/src/ATM/Account.java
+++ day04/src/ATM/Account.java
@@ -4,7 +4,7 @@ public class Account {
     /**
      * 成员变量
      */
-    private String carId;  // 卡号
+    private String cardId;  // 卡号
     private String username;  // 姓名
     private String password; // 密码
     private double money;// 金额
@@ -13,36 +13,12 @@ public class Account {
     public Account() {
     }
 
-    public String getCarId() {
-        return carId;
+    public String getCardId() {
+        return cardId;
     }
 
-    public void setCarId(String carId) {
-        this.carId = carId;
-    }
-
-    public double getQuotaMoney() {
-        return quotaMoney;
-    }
-
-    public void setQuotaMoney(double quotaMoney) {
-        this.quotaMoney = quotaMoney;
-    }
-
-    public Account(String carId, String username, String password, double money, double quotaMoney) {
-        this.carId = carId;
-        this.username = username;
-        this.password = password;
-        this.money = money;
-        this.quotaMoney = quotaMoney;
-    }
-
-    public String getcarId() {
-        return carId;
-    }
-
-    public void setcarId(String carId) {
-        this.carId = carId;
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
     }
 
     public String getUsername() {
@@ -68,4 +44,21 @@ public class Account {
     public void setMoney(double money) {
         this.money = money;
     }
+
+    public double getQuotaMoney() {
+        return quotaMoney;
+    }
+
+    public void setQuotaMoney(double quotaMoney) {
+        this.quotaMoney = quotaMoney;
+    }
+
+    public Account(String cardId, String username, String password, double money, double quotaMoney) {
+        this.cardId = cardId;
+        this.username = username;
+        this.password = password;
+        this.money = money;
+        this.quotaMoney = quotaMoney;
+    }
+
 }
--- day04/src/ATM/AtmSystem.java
+++ day04/src/ATM/AtmSystem.java
@@ -1,6 +1,7 @@
 package ATM;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class AtmSystem {
@@ -21,22 +22,141 @@ public class AtmSystem {
             Scanner scanner = new Scanner(System.in);
             int command = scanner.nextInt();
             switch (command) {
-                case 1 -> login(accounts, scanner);
-                case 2 -> register(accounts, scanner);
-                case 3 -> status = false;
-                default -> System.out.println("您输入的操作有误，请您重新输入！");
+                case 1: {
+                    login(accounts, scanner);
+                    break;
+                }
+                case 2: {
+                    register(accounts, scanner);
+                    break;
+                }
+                case 3: {
+                    status = false;
+                    break;
+                }
+                default: {
+                    System.out.println("您输入的操作有误，请您重新输入！");
+                    break;
+                }
             }
         }
     }
 
-    private static void register(ArrayList<Account> accounts, Scanner scanner) {
-    }
-
+    /**
+     * 登录
+     *
+     * @param accounts 全部集合
+     * @param scanner  输入
+     */
     private static void login(ArrayList<Account> accounts, Scanner scanner) {
-        System.out.println("请输入您的卡号");
-        String username=scanner.next();
-        System.out.println("请输入您的密码");
-        String password=scanner.next();
+
+        System.out.println("========== 欢迎您进入黑心银行ATM系统--登录 ============");
+        if (accounts.size() == 0) {
+            System.out.println("对不起，当前系统中,无任何账户，请先开户，再来登录~~");
+            return;
+        }
+                while (true) {
+            System.out.println("请输入您的卡号");
+            String cardId = scanner.next();
+            Account account = getAccountByCardId(accounts, cardId);
+            if (account == null) {
+                System.out.println("系统中不存在该账号");
+            } else {
+                while (true) {
+                    System.out.println("请输入您的密码");
+                    String password = scanner.next();
+                    if (!password.equals(account.getPassword())) {
+                        System.out.println("欢迎，" + account.getUsername() + "先生/女士，进入系统，您的卡号是:" + account.getCardId());
+
+                        // 登陆后的操作页面
+                        showUserCommand(account);
+                        return;
+
+                    } else {
+                        System.out.println("对不起，您输入的密码有误");
+                    }
+                }
+            }
+        }
 
     }
+
+    private static void showUserCommand(Account account) {
+
+    }
+
+    private static void register(ArrayList<Account> accounts, Scanner scanner) {
+        System.out.println("========== 欢迎您进入黑心银行ATM系统--注册 ============");
+        Account account = new Account();
+        System.out.println("请您输入您的姓名");
+        String username = scanner.next();
+        account.setUsername(username);
+        boolean status = true;
+        while (status) {
+            System.out.println("请您输入您的账户密码：");
+            String password_1 = scanner.next();
+            System.out.println("请您再次确认的账户密码：");
+            String password_2 = scanner.next();
+            if (password_1.equals(password_2)) {
+                account.setPassword(password_1);
+                status = false;
+            } else {
+                System.out.println("对不起，您两次输入的密码不一致，请您重新输入！");
+            }
+        }
+        System.out.println("请您输入您的账户每次限额：");
+        double money = scanner.nextDouble();
+        account.setQuotaMoney(money);
+        String cardId = getAutoCardId(accounts);
+        account.setCardId(cardId);
+        accounts.add(account);
+        System.out.println("恭喜您，" + username + "先生/女士，您开户成功，您的卡号是" + cardId);
+    }
+
+    /**
+     * 为账户生成8位与其他账户不同的号码
+     *
+     * @param accounts 全部账户集合
+     * @return 新的卡号
+     */
+    private static String getAutoCardId(ArrayList<Account> accounts) {
+
+        Random random = new Random();
+        boolean status = true;
+        String cardId = null;
+        while (status) {
+            cardId = "";
+            cardId += random.nextInt(9) + 1;
+            for (int i = 0; i < 7; i++) {
+                cardId += random.nextInt(10);
+            }
+
+            Account accountByCardId = getAccountByCardId(accounts, cardId);
+            if (accountByCardId == null) {
+                status = false;
+            }
+        }
+        return cardId;
+
+
+    }
+
+    /**
+     * 查看队列中有没有该卡号（根据卡号返回对象没有则空）
+     *
+     * @param accounts 全部账户集合
+     * @param cardID   卡号
+     * @return 用户对象/null
+     */
+    private static Account getAccountByCardId(ArrayList<Account> accounts, String cardID) {
+        for (Account account : accounts) {
+            if (account.getCardId().equals(cardID)) {
+                return account;
+            }
+        }
+
+        return null;
+    }
+
+
 }

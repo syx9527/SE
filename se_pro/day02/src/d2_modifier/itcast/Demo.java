8b2b7ba81514789080b8e70c93ad2bdd4be29a43
syx9527 <1275241305@q.com>
2022-03-10 09:38:01 +0800

    new day02

---
+++ se_pro/day02/src/d2_modifier/itcast/Demo.java
@@ -0,0 +1,12 @@
+package d2_modifier.itcast;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Fu f = new Fu();
+        // f.show1();
+        f.show2();
+        // f.show3();
+        f.show4();
+    }
+}
---
+++ se_pro/day02/src/d2_modifier/itcast/Fu.java
@@ -0,0 +1,34 @@
+package d2_modifier.itcast;
+
+public class Fu {
+
+    // 1.private：只能本类中访问
+    private void show1() {
+        System.out.println("private");
+    }
+
+    // 2.缺失：本类，同一个包下的类能访问
+    void show2() {
+        System.out.println("缺失");
+    }
+
+    // 3.protected：本类，同一个包下的类中，其他包下的子类
+
+    protected void show3() {
+        System.out.println("protected");
+    }
+
+    // public:任何地方都可以
+    public void show4() {
+        System.out.println("public");
+    }
+
+    public static void main(String[] args) {
+        Fu f = new Fu();
+        f.show1();
+        f.show2();
+        f.show3();
+        f.show4();
+    }
+
+}

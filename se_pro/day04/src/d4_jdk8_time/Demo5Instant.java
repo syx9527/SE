6ac341612a4cd01492cca28096c8c9b8084be3e5
syx <1275241305@qq.com>
2022-08-03 17:57:11 +0800

    jdk8_time

--- se_pro/day04/src/d4_jdk8_time/Demo5Instant.java
+++ se_pro/day04/src/d4_jdk8_time/Demo5Instant.java
@@ -1,4 +1,24 @@
 package d4_jdk8_time;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
+/**
+ * 时间戳
+ *
+ * @author 17447
+ */
 public class Demo5Instant {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        //系统此刻时间戳
+        Instant instant1 = Instant.now();
+
+        instant1.atZone(ZoneId.systemDefault());
+        System.out.println(instant1);
+
+
+    }
 }
